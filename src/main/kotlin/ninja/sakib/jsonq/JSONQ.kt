bc1855098917ed77873c70c685a2ba00d702c06d
Sakib Sami <s4kibs4mi@gmail.com>
2018-05-16 13:41:59 +0600

    Initial commit

---
+++ src/main/kotlin/ninja/sakib/jsonq/JSONQ.kt
@@ -0,0 +1,12 @@
+package ninja.sakib.jsonq
+
+/**
+ * := Coded with love by Sakib Sami on 5/16/18.
+ * := s4kibs4mi@gmail.com
+ * := www.sakib.ninja
+ * := Coffee : Dream : Code
+ */
+
+class JSONQ {
+
+}
